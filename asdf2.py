81071432c92d3e5853ca0e92315ad211991f9b18
jasparkatt <jasparkatt@yahoo.com>
2021-11-14 16:30:13 -0600

    making hay

--- asdf2.py
+++ asdf2.py
@@ -4,14 +4,16 @@ from tkinter import font
 from tkinter.constants import EW, NS, NSEW
 
 
-
-
-
 def topheader_frame(container):
     #add label frame
     top_labelframe = ttk.LabelFrame(container, text='ASDF Home', labelanchor='n', borderwidth=4)    
     # return the toplevel frame
     return top_labelframe
+
+# create our 'footer' frame at bottom
+def bottom_frame(container):
+    bottom_labelframe = ttk.LabelFrame(container, borderwidth=4, text='Footnotes & Thankyous', labelanchor='s')
+    return bottom_labelframe
 
 # create our left side frame
 def leftside_frame(container):
@@ -23,6 +25,8 @@ def leftside_frame(container):
 def rightside_frame(container):
     right_labelframe = ttk.LabelFrame(container, text='Place Data', labelanchor='n', borderwidth=4)
     return right_labelframe
+
+
 
 # create our main app window
 def create_main_window():
@@ -41,11 +45,15 @@ def create_main_window():
 
     #add our other frames
     top_frame = topheader_frame(root)
-    top_frame.pack(side='top',expand=0,fill='both',ipady=15,ipadx=15,pady=3,padx=3)
+    top_frame.pack(side='top',expand=0,fill='x',ipady=15,ipadx=15,pady=3,padx=3)
+    footer_frame = bottom_frame(root)
+    footer_frame.pack(side='bottom', expand=0, fill='x',
+                      ipady=15, ipadx=15, pady=3, padx=3)
     right_frame = rightside_frame(root)
-    right_frame.pack(side='right',expand=1,fill='both',ipady=2,ipadx=2,pady=3,padx=3)
+    right_frame.pack(side='right',expand=1,fill='both',ipady=3,ipadx=3,pady=3,padx=3)
     left_frame = leftside_frame(root)
-    left_frame.pack(side='left',expand=1,fill='both',ipady=2, ipadx=2, pady=3, padx=3)
+    left_frame.pack(side='left',expand=1,fill='both',ipady=3, ipadx=3, pady=3, padx=3)
+    
     # run mainlooop on the root 
     root.mainloop()
 # create our main window from the func above
