f7d4fef6e678c97848e53938f0e39aa50d24ed2d
jasparkatt <jasparkatt@yahoo.com>
2021-11-16 14:58:37 -0600

    dbl fuk yeah!!

--- asdf2.py
+++ asdf2.py
@@ -29,15 +29,29 @@ def leftside_frame(container):
         container, text='Water Data', labelanchor='n', borderwidth=4)
     left_labelframe.columnconfigure(0, weight=1)
     left_labelframe.columnconfigure(1, weight=1)
+
     # enter water body name
     streamlabel_text = tk.StringVar()
-    streamlabel = ttk.Label(left_labelframe, text='Water Fished(Name):',
-                            foreground='black', background='#FFEBF2', style='Data.TLabel')
-    streamlabel.grid(column=0, row=1, sticky=tk.EW)
-    streamlabel_entry = ttk.Entry(
-        left_labelframe, takefocus=0, cursor='hand1', textvariable=streamlabel_text)
+    streamlabel = ttk.Label(left_labelframe, text='Water Fished(Name):',foreground='black', background='#FFEBF2', style='Data.TLabel')
+    streamlabel.grid(column=0, row=1, sticky=tk.W)
+    streamlabel_entry = ttk.Entry(left_labelframe, takefocus=0, cursor='hand1', textvariable=streamlabel_text)
     streamlabel_entry.grid(column=1, row=1, sticky=tk.EW)
-    # add county selector
+    # add species type
+    def species_selected(event):
+        msg = f'You selected {specieslabel_text.get()}!'
+        showinfo(title='Result', message=msg)
+
+    # tkinter stuff for species select
+    species = ('Brown Trout', 'Rainbow Trout', 'Brook Trout', 'Steelhead', 'Lake Run Brown Trout', 'Carp', 'Smallmouth Bass',
+               'Largemouth Bass', 'Bluegill', 'Pumpkinseed', 'Perch', 'Walleye', 'Northern Pike', 'Musky', 'Bullhead')
+    specieslabel_text = tk.StringVar()
+    specieslabel = ttk.Label(left_labelframe,text='Select A Species:', foreground='black', background='#FFEBF2', style='Data.TLabel')
+    specieslabel.grid(column=0, row=2, sticky=tk.W,padx=5, pady=5, ipady=3, ipadx=3)
+    specieslabel_combo = ttk.Combobox(left_labelframe, textvariable=specieslabel_text)
+    specieslabel_combo['values'] = species
+    specieslabel_combo['state'] = 'readonly'
+    specieslabel_combo.grid(column=1, row=2, sticky=tk.EW)
+    specieslabel_combo.bind('<<ComboboxSelected>>', species_selected)
 
     return left_labelframe
 
@@ -50,24 +64,30 @@ def rightside_frame(container):
     right_labelframe.columnconfigure(0, weight=1)
     right_labelframe.columnconfigure(1, weight=1)
     # add species select chunk
-
-    def species_selected(event):
-        msg = f'You selected {specieslabel_text.get()}!'
+    def county_selected(event):
+        msg = f'You selected {countylabel_text.get()}!'
         showinfo(title='Result', message=msg)
 
-    # tkinter stuff for species select
-    species = ('Brown Trout', 'Rainbow Trout', 'Brook Trout', 'Steelhead', 'Lake Run Brown Trout', 'Carp', 'Smallmouth Bass',
-               'Largemouth Bass', 'Bluegill', 'Pumpkinseed', 'Perch', 'Walleye', 'Northern Pike', 'Musky', 'Bullhead')
-    specieslabel_text = tk.StringVar()
-    specieslabel = ttk.Label(right_labelframe,
-        text='Select A Species:', foreground='black', background='#FFEBF2', style='Data.TLabel')
-    specieslabel.grid(column=0, row=1, sticky=tk.W,
-                      padx=5, pady=5, ipady=3, ipadx=3)
-    specieslabel_combo = ttk.Combobox(right_labelframe, textvariable=specieslabel_text)
-    specieslabel_combo['values'] = species
-    specieslabel_combo['state'] = 'readonly'
-    specieslabel_combo.grid(column=1, row=1, sticky=tk.EW, padx=5, pady=5)
-    specieslabel_combo.bind('<<ComboboxSelected>>', species_selected)
+    countylabel_text = tk.StringVar()
+    county = ('Adams','Ashland','Barron','Bayfield','Brown','Buffalo','Burnett',
+                       'Calumet','Chippewa','Clark','Columbia','Crawford','Dane','Dodge',
+                       'Door','Douglas','Dunn','Eau Claire','Florence','Fond du Lac','Forest',
+                       'Grant','Green','Green Lake','Iowa','Iron','Jackson','Jefferson','Juneau',
+                       'Kenosha','Kewaunee','La Crosse','Lafayette','Langlade','Lincoln','Manitowoc',
+                       'Marathon','Marinette','Marquette','Menominee','Milwaukee','Monroe','Oconto',
+                       'Oneida', 'Outagamie', 'Ozaukee', 'Pepin', 'Pierce', 'Polk', 'Portage', 'Price',
+                       'Racine','Richland','Rock','Rusk','Saint Croix','Sauk','Sawyer','Shawano','Sheboygan',
+                       'Taylor','Trempealeau','Vernon','Vilas','Walworth','Washburn','Washington',
+                       'Waukesha', 'Waupaca', 'Waushara', 'Winnebago', 'Wood'
+                       )
+    countylabel = ttk.Label(right_labelframe, text='Enter County Name:',foreground='black', background='#FFEBF2', style='Data.TLabel')
+    countylabel.grid(column=0, row=1, sticky=tk.W,padx=5, pady=5, ipady=3, ipadx=3)
+    countylabel_combo = ttk.Combobox(right_labelframe, textvariable=countylabel_text)
+    countylabel_combo['values'] = county
+    countylabel_combo['state'] = 'readonly'
+    countylabel_combo.grid(column=1, row=1, sticky=tk.EW, padx=5, pady=5)
+    countylabel_combo.bind('<<ComboboxSelected>>', county_selected)
+    
     #
     return right_labelframe
 
@@ -76,18 +96,18 @@ def rightside_frame(container):
 def create_main_window():
     # create root
     root = tk.Tk()
-    root.title('ASDF Log')
+    root.title('ASDF')
     root.geometry('600x600+15+15')
     root.iconbitmap('./assets/favicon_sa.ico')
     root.resizable(True, True)
     root.configure(bg='#FFEBF2')
     # create our style library
     style = ttk.Style()
-    style.theme_use('winnative')
+    style.theme_use('vista')
     # the below is automagically applied to any labelframe label txt
-    style.configure('TLabelframe.Label', font=('Red Hat Text', 11))
-    style.configure('Bottom.TLabelframe.Label', font=('Book Antiqua', 9))
+    style.configure('TLabelframe.Label', font=('Red Hat Text', 11))    
     style.configure('TLabel', font=('Fira Mono', 9))
+    style.configure('Bottom.TLabelframe.Label', font=('Book Antiqua', 9))
     style.configure('Data.TLabel', font=('Roboto Mono', 9))
     # add our other frames
     # maintain this order of adding frames to keep footer correct
